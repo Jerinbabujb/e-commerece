e53c64e4e26896f9a5b42186b32f20be38bf6b5e
JERIN BABU <114816204+Jerinbabujb@users.noreply.github.com>
2023-11-23 18:41:38 +0530

    urls.py

--- Shopping1/Shopping/shopping_pro/shopping_app/urls.py
+++ Shopping1/Shopping/shopping_pro/shopping_app/urls.py
@@ -5,6 +5,6 @@ app_name='shopping_app'
 urlpatterns = [
 
     path('',views.home,name='home'),
-    path('<slug:c_slug>/',views.home,name='pro_by_cat'),
+    path('home/<slug:c_slug>/',views.home,name='pro_by_cat'),
     path('<slug:c_slug>/<slug:p_slug>/',views.proDet,name='proDet')
 ]
